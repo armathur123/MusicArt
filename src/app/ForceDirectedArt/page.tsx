5a87c0187f8aa926cdd4c992db7d8bb73faca363
Romey Bell <romeybell@Romeys-MacBook-Air.local>
2023-12-08 00:21:29 -0500

    Text nodes appear on hover

--- src/app/ForceDirectedArt/page.tsx
+++ src/app/ForceDirectedArt/page.tsx
@@ -7,8 +7,6 @@ import * as d3 from 'd3';
 import { useEffect } from 'react';
 import Circle from '../_components/_circle/Circle';
 
-type ArtistLinkType = (Artist & d3.SimulationNodeDatum);
-
 type ArtistNodeType = (Artist & d3.SimulationNodeDatum);
 
 const ForceDirectedArt = () => {
@@ -16,7 +14,7 @@ const ForceDirectedArt = () => {
         method: 'GET',
         params: {
             time_range: 'medium_term',
-            limit: '20'
+            limit: '10'
         }
     });
     const width = 900, height = 900;
@@ -65,19 +63,48 @@ const ForceDirectedArt = () => {
             .attr('stroke-opacity', 0.6)      
             .attr('stroke-width', d => (d.commonGenres.length ** 2));
 
+        const showTextOnNode = (show: boolean, id: string) => {
+            const transition = d3.transition()
+                .duration(500)
+                .ease(d3.easeLinear);
+
+            if (show) {
+                d3.select('#text' + id)
+                    .transition(transition)
+                    .style('opacity', '1');
+            }
+            else {
+                d3.select('#text' + id)
+                    .transition(transition)
+                    .style('opacity', '0');
+            }
+        };
+
         const artistNodes = svg
             .selectAll('image')
             .data(nodes)
             .join('image')
+            .attr('id', d => 'node' + d.id)
             .attr('width', (d) => d.popularity * 1.4)
             .attr('height', (d) => d.popularity * 1.4)
             .attr('xlink:href', (d) => d.images[0].url)
             .attr('style', 'clip-path: circle(40%);')
             .attr('transform', (d) => `translate(-${d.popularity * 1.4 / 2}, -${d.popularity * 1.4 / 2})`)
-            .on('mouseover', function (event, d) {
-                d3.select(this).transition()
-                    .duration(50)
-                    .attr('opacity', '.5');
+            .on('mouseover', (event, d) => {
+                // how to select 'this' with anonymous arrow function :)
+                // d3.select(event.target).transition()
+                //     .duration(50)
+                //     .attr('opacity', ''); 
+
+                showTextOnNode(true, d.id);
+                // const connectionCacheKeys = Object.keys(connectionCache);
+                // for (const connectionCacheKey of connectionCacheKeys) {
+                //     const containsID = connectionCacheKey.
+                // }
+                // show links only on hover || hide other links on hover ?
+            })
+            .on('mouseout', (event, d) => {
+                showTextOnNode(false, d.id);
             });
 
             
@@ -85,19 +112,25 @@ const ForceDirectedArt = () => {
             .selectAll('text')
             .data(nodes)
             .join('text')
+            .attr('id', d => 'text' + d.id)
+            .text((d) => d.name)
             .attr('text-anchor', 'middle')
             .attr('stroke', 'white')
             .attr('fill', 'white')
             .attr('stroke-width', '1px')
             .attr('alignment-baseline', 'middle')
             .attr('lengthAdjust', 'spacingAndGlyphs')
-            .attr('font-size', (d) => 14)
-            .attr('font-weight', '200')
-            .attr('textLength', (d) => d.popularity * .7)
-            .text((d) => d.name);
+            .style('font-size', (d) => 14)
+            .style('font-weight', '200')
+            .style('textLength', (d) => d.popularity * .7)
+            .style('opacity', '0')
+            .on('mouseover', (event, d) => {
+                showTextOnNode(true, d.id);
+            })
+            .on('mouseout', (event, d) => {
+                showTextOnNode(false, d.id);
+            });
 
-
-        console.log(links);
             
         const update = () => {
             artistNodes
@@ -114,19 +147,20 @@ const ForceDirectedArt = () => {
                 .attr('y', (d) => d.y!);
 
             linkNodes
-                .attr('x1', d => d.source.x!)
-                .attr('y1', d => d.source.y!)
-                .attr('x2', d => d.target.x!)
-                .attr('y2', d => d.target.y!);
+                .attr('x1', (d: any) => d.source.x!)
+                .attr('y1', (d: any) => d.source.y!)
+                .attr('x2', (d: any) => d.target.x!)
+                .attr('y2', (d: any) => d.target.y!);
     
 
         };
 
-        const simulation = d3.forceSimulation<(Artist & d3.SimulationNodeDatum)>(nodes)
-            .force('link', d3.forceLink<(Artist & d3.SimulationNodeDatum), {source: string, target: string}>(links).id((d) => d.id))
+        const simulation = d3.forceSimulation<ArtistNodeType>(nodes)
+            .force('link', d3.forceLink<ArtistNodeType, d3.SimulationLinkDatum<ArtistNodeType>>(links).id((d) => d.id))
             .force('center', d3.forceCenter(width / 2, height / 2))
-            .force('charge', d3.forceManyBody().strength(-20))
+            // .force('charge', d3.forceManyBody().strength(-5))
             .force('collide', d3.forceCollide<(Artist & d3.SimulationNodeDatum)>().radius((d) => {return d.popularity * .8;}))
+            //custom force to keep stuff within the boundaries (not sure if this is necessary, look into it)
             .on('tick', update);
 
     }, [data]);
